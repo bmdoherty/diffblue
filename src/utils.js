81a3194cca5463770ac93841fdcc55f6eb28a1db
BM Doherty <bmdoherty@sidewinder.co.uk>
2017-08-07 01:25:17 +0100

    fix codeclimate issue with semi-colons

--- src/utils.js
+++ src/utils.js
@@ -1,28 +1,28 @@
 function compare(a,b) {
   if (a.directory < b.directory)
-    return -1;
+    return -1
   if (a.directory > b.directory)
-    return 1;
-  return 0;
+    return 1
+  return 0
 }
 
 export const processData = (data) => {
 
-  var newData = [];
+  var newData = []
   Object.keys(data).forEach(function(key, index) {
-    var item = {};
-    var arr = key.split('/');
+    var item = {}
+    var arr = key.split('/')
     
-    item.key = key;
-    item.fileName = arr.pop();
-    item.directory = arr.join('/') + '/'; 
-    item.linesCovered = data[key][0]; 
-    item.linesTotal = data[key][1]; 
+    item.key = key
+    item.fileName = arr.pop()
+    item.directory = arr.join('/') + '/'
+    item.linesCovered = data[key][0]
+    item.linesTotal = data[key][1]
 
-    newData.push(item);  
+    newData.push(item)
   })
 
-  return newData.sort(compare);
+  return newData.sort(compare)
 }
 
 export const filesInfo = (files) => {
@@ -31,22 +31,22 @@ export const filesInfo = (files) => {
     filesInfo.files = files.length   
 
     filesInfo.linesCovered = files
-      .map(function(f) { return f.linesCovered; })
-      .reduce(function(p, c) { return p + c; });
+      .map(function(f) { return f.linesCovered })
+      .reduce(function(p, c) { return p + c })
 
     filesInfo.linesTotal = files
-      .map(function(f) { return f.linesTotal; })
-      .reduce(function(p, c) { return p + c; });            
+      .map(function(f) { return f.linesTotal })
+      .reduce(function(p, c) { return p + c })          
         
-    filesInfo.percent = parseInt((( filesInfo.linesCovered / filesInfo.linesTotal ) * 100).toFixed(0), 10);
+    filesInfo.percent = parseInt((( filesInfo.linesCovered / filesInfo.linesTotal ) * 100).toFixed(0), 10)
 
-    filesInfo.directory = files[0].directory;
+    filesInfo.directory = files[0].directory
 
-    return filesInfo;
+    return filesInfo
   }
 
 export const directories = (data) => {
-    var lastDirectory = '';
+    var lastDirectory = ''
     var directories = []
 
     data.forEach(function(item, index) {
@@ -55,11 +55,11 @@ export const directories = (data) => {
       if (file.directory !== lastDirectory) {
         let files = data.filter(function (f) {
           if (f.directory === file.directory ){
-            return f;
+            return f
           }
 
           return null
-        });  
+        })
         
         directories.push( filesInfo(files) )
       }
@@ -74,9 +74,9 @@ export const directories = (data) => {
     var files = data.filter(function (f) {
       if (f.directory === directory ) {
         return f
-      };
+      }
       return null
-    });
+    })
 
-    return files;
+    return files
   }
